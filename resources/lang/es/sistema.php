3353fe99b7a3aa06777742e8469f444de21f0b27
SebastiaN <odm_sd@hotmail.com>
2018-10-09 15:45:20 -0500

    correccoin de idioma

---
+++ resources/lang/es/sistema.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Lenguaje procio de alumni
+    |--------------------------------------------------------------------------
+    */
+
+    'cargando'             => 'Cargando . . .',
+    'empresas_registradas' => 'Empresas Registradas',
+    'between'              => [
+        'numeric' => 'El campo :attribute debe ser un valor entre :min y :max.',
+        'file'    => 'El archivo :attribute debe pesar entre :min y :max kilobytes.',
+        'string'  => 'El campo :attribute debe contener entre :min y :max caracteres.',
+        'array'   => 'El campo :attribute debe contener entre :min y :max elementos.',
+    ],
+    'labels'  =>[
+            'estudiantes'   =>  'Estudiantes',
+            'empresas'      =>  'Organizaciones',
+            'publicaciones' =>  'Publicaciones',
+    ],
+    'validacion'    =>[
+        'feria_exito'    =>  'Se ha registrado exitosamente',
+
+    ],
+    'titulos'   =>[
+        'registro_feria'    =>  'Registro Feria',
+    ]
+];
