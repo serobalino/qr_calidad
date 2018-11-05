ca2820a003ed531b8bb6d06e0154d642c03402c1
SebastiaN <odm_sd@hotmail.com>
2018-11-05 18:01:37 -0500

    link de facebook y contacto

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
+                    <span class="text-muted d-none d-md-block">Con el apoyo de la Dirección de Aseguramiento de la Calidad - PUCE</span>
                 </a>
             </div>
         </nav>
@@ -20,6 +21,10 @@
         <main class="py-4">
             @yield('content')
         </main>
+    </div>
+    <div class="container">
+        <a href="https://www.facebook.com/consupepuce/" class="btn btn-link" target="_blank"><span class="fa fa-facebook-square"></span> fanpage</a>
+        <a href="https://consupe.com/contacto/" class="btn btn-link" target="_blank"><span class="fa fa-phone"></span> Contacto</a>
     </div>
     <script src="{{ asset('js/app.js') }}" defer></script>
 </body>
