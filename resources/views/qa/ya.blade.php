60bfefbf12d5a06a02f4d2e14c27f00916cad46c
SebastiaN <odm_sd@hotmail.com>
2018-11-09 16:08:52 -0500

    link regresar al buscador de servicios

--- resources/views/qa/ya.blade.php
+++ resources/views/qa/ya.blade.php
@@ -6,5 +6,8 @@
             <hr>
             <p class="mb-0">No puedes calificar el mismo servicio varias veces</p>
         </div>
+        <div class="row">
+            <a class="btn btn-link" type="button" href="{{route('qa.formulario')}}"><i class="fa fa-search" aria-hidden="true"></i> Buscar Servicio</a>
+        </div>
     </div>
 @endsection
